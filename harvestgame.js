1035fb380d3b1b221db3588e163bb1857b70df6e
a10abrams <108836278+a10abrams@users.noreply.github.com>
2022-11-07 11:50:33 -0500

    JS overhaul
    I have a terrible habit of brute-forcing things that just don't really work. This rework of the JS is actually what I need!

--- harvestgame.js
+++ harvestgame.js
@@ -1,67 +1,99 @@
-window.addEventListener('load', function(){
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    canvas.width = 900;
-    canvas.height = 506;
+/* create canvas */
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
 
-    /* reads keystrokes and handles if multiple are pressed at once */
-    class InputReader {
-        constructor(){
-            this.keys = [];
-            window.addEventListener('keydown', e => {
-                if ((   e.key === 'ArrowDown' ||
-                        e.key === 'ArrownDown' ||
-                        e.key === 'ArrowLeft' ||
-                        e.key === 'ArrowRight')
-                        && this.keys.indexOf(e.key) === -1){
-                    this.keys.push(e.key);
-                }
-            });
-            window.addEventListener('keyup', e => {
-                if (    e.key === 'ArrowDown' ||
-                        e.key === 'ArrowUp' ||
-                        e.key === 'ArrowLeft' ||
-                        e.key === 'ArrowRight'){
-                    this.keys.splice(this.keys.indecOf(e.key), 1);
-                }
-            });
-        }
-    }
+/* globals */
+const currentBg = 0;
+const bg = ['wgbackground_01.png','wgbackground_02.png','wgbackground_03.png','wgbackground_04.png','wgbackground_05.png','wgbackground_07.png'];
+let count = 0;
+let image = new Image();
 
-    class Player {
-        constructor(gameWidth, gameHeight){
-            this.gameWidth = gameWidth;
-            this.gameHeight = gameHeight;
-            this.x = 0;
-            this.y = 0;
-        }
-        draw(context){
-            context.fillstyle = 'white';
-            context.fillRect(this.x, this.y, this.width, this.hegiht);
-        }
-    }
-    /*
-    class Background {
-
-    }
-
-    class Enemy {
-
-    }
-
-    function enemybehavior(){
-
-    }
-
-    function displaystatustext(){
-
-    }
-    */
-    const input = new InputReader();
-    const player = new Player(canvas.width, canvas.height);
-    player.draw(ctx)
-
-    function animate(){
-
-    }
+window.addEventListener("keydown",function(e){
+    window.key = e.keyCode;
+    console.log(window.key)
 });
+window.addEventListener("keyup",function(e){
+    this.window.key = false;
+    console.log(window.key);
+});
+
+canvas.width = 900;
+canvas.height = 500;
+
+const usedbg = [];
+
+const playersprites = "character_witchresize.png";
+image.src = playersprites;
+
+/*this may need to be updated */
+let bgImg = new Image();
+bgImg.width = window.innerWidth;
+bgImg.src = bg[count];
+
+count+=1;
+
+usedbg.push(bgImg.src);
+
+function game_object(x,y, currentFrame, rows, columns,imgWidth, imgHeight,srcX,
+    srcY, is_person, is_interact){
+   
+   this.x = x;
+     this.y = y;
+     this.currentFrame = currentFrame;
+     this.rows = rows;
+     this.columns = columns;
+     this.imgWidth = imgWidth;
+     this.imgHeight = imgHeight;
+     this.srcX = srcX;
+     this.srcY = srcY;
+     const rowSize = this. imgWidth/this.columns;
+     const colSize = this.imgHeight/this.rows;
+     this.srcY = 1*colSize;
+     this.draw = function (){
+       
+       context.drawImage(bgImg,0,0);
+       context.drawImage(image,this.srcX,this.srcY,rowSize, colSize,this.x,this.y,rowSize, colSize);
+       context.clearRect(canvas.width,canvas.height,0,0);
+  
+     }
+   this.update = function(){
+      if(window.key == 39 && this.x <= bgImg.width+365){
+         this.srcY = 1*colSize;
+         this.currentFrame = ++this.currentFrame % this.columns;
+         console.log(x,bgImg.width+500);
+         this.x = this.x+20;
+         }
+      if(window.key == 37 && this.x > 0 ){
+         this.currentFrame =  ++this.currentFrame % this.columns;
+         this.srcY = 0*colSize;
+         this.x = this.x-20;
+         }
+      this.srcX = this.currentFrame * rowSize;
+      if (this.x >= bgImg.width - 200 && window.key == 32) {
+          switch (count) {
+            case 1:
+              bgImg.src = bg[count];
+              count+=1;
+              this.x=50;
+              break;
+           case 2:
+             bgImg.src = bg[count];
+             count+=1;
+             console.log(table);
+             this.x=0;
+             break;
+          default:
+            this.x=0;
+            break;
+          }
+             }
+     }
+ 
+ }
+ var main_player = new game_object(500, 230, 0, 2, 4, 800, 600, true, true);
+ 
+ setInterval(function(){
+   main_player.update();
+   context.clearRect(0,0,canvas.width,canvas.height)
+   main_player.draw();
+ },250);
